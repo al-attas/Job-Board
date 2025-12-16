9ec94b97491e99262fc30561583de381c0743853
Abdulrahman Alattas <abdulrahman_alattas@outlook.com>
2025-12-16 14:47:40 +0300

    add static file and added static template

--- project/settings.py
+++ project/settings.py
@@ -118,3 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    "/var/www/static/",
+]
+
+MEDIA_URL = 'media/'
